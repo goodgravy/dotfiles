b8c8de690be73f820f9a45743a9bc7cd92949d1e
James Brady <james@jmsbrdy.com>
2017-05-29 11:58:42 +0200

    feat: re-enable "hold down for accents"

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -5,9 +5,6 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
