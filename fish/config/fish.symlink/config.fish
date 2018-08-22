f372ca01fad93eccb0270b55e11fff77f2f5b0dd
James Brady <james@jmsbrdy.com>
2018-08-22 13:36:27 +0200

    Google cloud SDK path changes

--- fish/config/fish.symlink/config.fish
+++ fish/config/fish.symlink/config.fish
@@ -3,3 +3,9 @@ set -x PATH $PATH $dotfiles_dir/bin
 for file in $dotfiles_dir/*/*.fish
   source $file
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/james/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/james/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/james/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
