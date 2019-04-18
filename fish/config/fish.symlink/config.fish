98fe2c0f3074553963d42e76b1fc20f16a148dcb
James Brady <james@jmsbrdy.com>
2019-04-18 11:54:21 +0200

    chore: fish path updates for serverless, sqlite, and mysql

--- fish/config/fish.symlink/config.fish
+++ fish/config/fish.symlink/config.fish
@@ -9,3 +9,12 @@ if [ -f '/Users/james/Downloads/google-cloud-sdk/path.fish.inc' ]; if type sourc
 
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
