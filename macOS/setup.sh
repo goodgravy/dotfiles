6a77ff6c949eb1ff3537061b34cf9e1e1cde7c41
goodgravy <james@jmsbrdy.com>
2019-08-13 11:41:45 +0000

    feat: only run MacOS customisers on MacOS

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -5,23 +5,27 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+if [ "$(uname)" == "Darwin" ]; then
 
-# Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+  # Use AirDrop over every interface. srsly this should be a default.
+  defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+  
+  # Always open everything in Finder's list view. This is important.
+  defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+  
+  # Show the ~/Library folder.
+  chflags nohidden ~/Library
+  
+  # Set a really fast key repeat.
+  defaults write NSGlobalDomain KeyRepeat -int 2
+  defaults write NSGlobalDomain InitialKeyRepeat -int 15
+  
+  # Set the Finder prefs for showing a few different volumes on the Desktop.
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+  
+  # Run the screensaver if we're in the bottom-left hot corner.
+  defaults write com.apple.dock wvous-bl-corner -int 5
+  defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# Show the ~/Library folder.
-chflags nohidden ~/Library
-
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+fi
