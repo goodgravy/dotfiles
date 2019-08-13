231477e4414757e8c1e7a55cc7c34d26282ddac1
goodgravy <james@jmsbrdy.com>
2019-08-13 11:42:18 +0000

    chore: split config.fish into conf.d files

---
+++ fish/config/fish.symlink/conf.d/00-path.fish
@@ -0,0 +1,3 @@
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
+set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
