68e0ed6cc7066261eca335d1c08a545f1d8a2808
James Brady <james@jmsbrdy.com>
2019-08-26 14:45:32 +0200

    feat: fnm for node management

---
+++ fish/config/fish.symlink/config.fish
@@ -0,0 +1,3 @@
+# https://github.com/Schniz/fnm
+# fnm
+fnm env --multi | source
--- node/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-set -o pipefail
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
