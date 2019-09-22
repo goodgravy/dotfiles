4aa96b201039c3c9feba7d7951ee92f64405dc77
goodgravy <james@jmsbrdy.com>
2019-09-22 22:31:40 +0200

    chore: remove global fish paths

--- fish/config/fish.symlink/conf.d/00-path.fish
+++
@@ -1,3 +0,0 @@
-set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
