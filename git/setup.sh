225ac4f32372d9b3246fe29057cd85f4794372d7
James Brady <james@jmsbrdy.com>
2020-06-18 14:20:35 +0000

    chore: move git aliases to setup

--- git/setup.sh
+++ git/setup.sh
@@ -28,3 +28,38 @@ then
   echo "created $script_dir/gitconfig.local.symlink"
 fi
 
+echo 'Setting up git alias functions'
+function glog
+  git log \
+    --graph --abbrev-commit --date=relative \
+    --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' \
+    $argv
+end
+
+function gp
+  git push origin HEAD $argv
+end
+
+function gd
+  git diff $argv
+end
+
+function gc
+  git commit $argv
+end
+
+function gco
+  git checkout $argv
+end
+
+function gs
+  git status -sb $argv
+end
+
+function gap
+  git add --patch $argv
+end
+
+function gcp
+  git checkout --patch $argv
+end
