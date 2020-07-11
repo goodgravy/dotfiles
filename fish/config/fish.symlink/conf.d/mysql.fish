45f9a6e0bf9f80e7386f6b8c47cb6c6c73f98e67
James Brady <james@jmsbrdy.com>
2020-07-11 15:55:11 +0000

    feat: add .local/bin to path

--- fish/config/fish.symlink/conf.d/mysql.fish
+++ fish/config/fish.symlink/conf.d/mysql.fish
@@ -1,6 +1,6 @@
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/opt/mysql-client/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.linuxbrew/opt/mysql-client/bin" $fish_user_paths
 
-set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/mysql-client/lib"
-set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/mysql-client/include"
+set -gx LDFLAGS "-L$HOME/.linuxbrew/opt/mysql-client/lib"
+set -gx CPPFLAGS "-I$HOME/.linuxbrew/opt/mysql-client/include"
 
-set -gx PKG_CONFIG_PATH "/home/linuxbrew/.linuxbrew/opt/mysql-client/lib/pkgconfig"
+set -gx PKG_CONFIG_PATH "$HOME/.linuxbrew/opt/mysql-client/lib/pkgconfig"
---
+++ fish/config/fish.symlink/conf.d/python.fish
@@ -0,0 +1,1 @@
+set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
