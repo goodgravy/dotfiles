9a2de3754906ef356c4b92dc1f10f42619242929
James Brady <james@jmsbrdy.com>
2022-05-05 19:33:29 +0200

    chore: add asdf and conda shell setup

--- fish/config/fish.symlink/conf.d/python.fish
+++ fish/config/fish.symlink/conf.d/python.fish
@@ -1,2 +1,6 @@
 set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/Library/Python/3.9/bin" $fish_user_paths
+
+# >>> conda initialize >>>
+eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
