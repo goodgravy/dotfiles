6e08cc2d7be5a297539a9e9977a802a17bd759d7
James Brady <james@jmsbrdy.com>
2023-12-15 14:33:02 +0100

    chore: tidy up fish aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,17 +1,8 @@
 alias glog "git log --graph --abbrev-commit --date=relative --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset'"
-
 alias gp "git push origin HEAD"
-
 alias gd "git diff"
-
 alias gc "git commit"
-
 alias gco "git checkout"
-
 alias gs "git status -sb"
-
 alias gap "git add --patch"
-
 alias gcp "git checkout --patch"
-
-alias webc "convox run web 'APPOPTICS_SERVICE_KEY=lalala rails c' -r teespring/rails-production -a teespring"
