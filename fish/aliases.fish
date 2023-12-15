ad2f8ea0910c03f74380d89cf3775ce6d82ab020
James Brady <james@jmsbrdy.com>
2023-12-15 14:33:50 +0100

    chore: move git aliases to git dir

--- fish/aliases.fish
+++
@@ -1,8 +0,0 @@
-alias glog "git log --graph --abbrev-commit --date=relative --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset'"
-alias gp "git push origin HEAD"
-alias gd "git diff"
-alias gc "git commit"
-alias gco "git checkout"
-alias gs "git status -sb"
-alias gap "git add --patch"
-alias gcp "git checkout --patch"
